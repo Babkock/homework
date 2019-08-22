0d791e2cfc529f16fa4ccea341abb449cc77b0ab
Tanner Babcock <babkock@gmail.com>
2019-08-21 20:16:21 -0500

    Converting lame ass Web 0.5 structure to my TBcom namespace

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/* HOMEWORK
+ *
+ *      /homework/index
+ *
+ * Copyright (c) 2017-2020 Tanner Babcock.
+*/
+namespace TBcom;
+require_once("../resources/lib/TBcom.php");
+require_once("../resources/lib/Log.php");
+
+$hw = new Build\Page("homework.header", "homework.index", "homework.footer");
+$hw->init("Tanner Babcock Homework", "Homework assignments for Tanner Babcock.", Codes\Portfolio);
+
+$hw->header->breadcrumbs([
+	"Homework" => "https://tannerbabcock.com/homework/index"
+]);
+$hw->output();
+
