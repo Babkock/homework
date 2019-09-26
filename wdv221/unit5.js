5dd608ad3ebc9cc3ea529cc75c3ba0a7fb22d182
Tanner Babcock <babkock@gmail.com>
2019-09-25 23:03:20 -0500

    Unit 5

--- wdv221/unit5.js
+++ wdv221/unit5.js
@@ -1,5 +1,5 @@
 function redpara() {
-	document.getElementById("redpara").style.color = "red";
+	document.getElementById("redpara").style.color = "#ff0000";
 }
 
 function changeTopic(val) {
@@ -10,7 +10,15 @@ function changeFont(val) {
 	document.getElementById("fontpara").style.fontFamily = val;
 }
 
+function toggleSale() {
+	if (document.getElementById("sale").style.fontSize == "24px")
+		document.getElementById("sale").style.fontSize = "14px";
+	if (document.getElementById("sale").style.fontSize == "14px")
+		document.getElementById("sale").style.fontSize = "24px";
+}
+
 document.getElementById("redpara").addEventListener("click", redpara);
 document.getElementById("button").addEventListener("click", changeTopic("left"));
-document.getElementById("fontpara").addEventListener("click", changeFont("Times New Roman"));
+document.getElementById("fontpara").addEventListener("click", changeFont("Times"));
+document.getElementById("sale").addEventListener("click", toggleSale);
 
