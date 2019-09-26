17a721e8ac8d4f6670229d61b509694a29a61d10
Tanner Babcock <babkock@gmail.com>
2019-09-25 19:24:31 -0500

    Lab exam

---
+++ wdv221/lab/labExam.js
@@ -0,0 +1,29 @@
+var longDays = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+	var longMonths = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+var customerName = "Please Sign In";
+
+function enlargeImage(inImage)	{
+	//alert("inside enlargeImage()" );
+	inImage.style.transform = "scale(2,2)";
+}
+
+function originalSizeImage(inImage)	{
+	//alert("inside originalSizeImage()");	
+	inImage.style.transform = "scale(1,1)";					
+}
+
+function signin() {
+	customerName = prompt("Please enter your name: ");
+	document.getElementById("customer").innerHTML = customerName;
+}
+
+document.getElementById("signin").addEventListener("click", signin);
+document.getElementById("picHtml").addEventListener("mouseOver", enlargeImage(this));
+document.getElementById("picHtml").addEventListener("mouseOut", originalSizeImage(this));
+
+var t = new Date();
+
+var today = document.getElementById("today");
+today.innerHTML = longDays[t.getDay()] + ", " + longMonths[t.getMonth()] + " " + t.getDate() + ", " + (t.getYear() + 1899);
+
