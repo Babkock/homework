b2cf1be83c88b97305010483c6b51194a8ccb2ff
Tanner Babcock <babkock@gmail.com>
2020-08-27 13:37:38 -0700

    Starting Intro to PHP and Advanced Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,48 @@
 $(document).ready(function() {
+	$("#wdv341-toggle").on("click", function() {
+		$(".wdv341").show();
+		$(".wdv321").hide();
+		$(".wdv221").hide();
+		$(".wdv205").hide();
+		$(".wdv131").hide();
+		$(".wdv101").hide();
+	});
+	$("#wdv321-toggle").on("click", function() {
+		$(".wdv341").hide();
+		$(".wdv321").show();
+		$(".wdv221").hide();
+		$(".wdv205").hide();
+		$(".wdv131").hide();
+		$(".wdv101").hide();
+	});
 	$("#wdv221-toggle").on("click", function() {
 		$(".wdv221").show();
+		$(".wdv341").hide();
+		$(".wdv321").hide();
 		$(".wdv205").hide();
 		$(".wdv131").hide();
 		$(".wdv101").hide();
 	});
 	$("#wdv205-toggle").on("click", function() {
 		$(".wdv205").show();
+		$(".wdv341").hide();
+		$(".wdv321").hide();
 		$(".wdv221").hide();
 		$(".wdv131").hide();
 		$(".wdv101").hide();
 	});
 	$("#wdv131-toggle").on("click", function() {
 		$(".wdv131").show();
+		$(".wdv341").hide();
+		$(".wdv321").hide();
 		$(".wdv221").hide();
 		$(".wdv205").hide();
 		$(".wdv101").hide();
 	});
 	$("#wdv101-toggle").on("click", function() {
 		$(".wdv101").show();
+		$(".wdv341").hide();
+		$(".wdv321").hide();
 		$(".wdv221").hide();
 		$(".wdv205").hide();
 		$(".wdv131").hide();
--- index.php
+++ index.php
@@ -3,14 +3,14 @@
  *
  *      /homework/index
  *
- * Copyright (c) 2017-2020 Tanner Babcock.
+ * Copyright (c) 2017-2021 Tanner Babcock.
 */
 namespace TBcom;
 require_once("../resources/lib/TBcom.php");
 require_once("../resources/lib/Log.php");
 
 $hw = new Build\Page("homework.header", "homework.index", "homework.footer");
-$hw->init("Tanner Babcock Homework", "Homework assignments for Tanner Babcock.", Codes\Portfolio);
+$hw->init("Tanner Babcock Fall 2020", "Homework assignments for Tanner Babcock.", Codes\Portfolio);
 
 $hw->middle->replace("EMAIL", "tababcock@dmacc.edu");
 
