364947416efd2127009d4af2d6be4f282131ff63
Tanner Babcock <babkock@gmail.com>
2020-09-03 18:34:05 -0700

    Moved jQuery script, started PHP assignment

---
+++ wdv341/unit4functions.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+	Unit 4: PHP Functions
+	Tanner Babcock
+	September 4, 2020
+-->
+<html lang="en">
+<head>
+	<title>Unit 4: PHP Functions</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+<body>
+<?php
+
+function dateToString($d) {
+	return date("m-d-Y", $d);
+}
+
+function intDateToString($d) {
+	return date("d-m-Y", $d);
+}
+
+function checkString($str) {
+	
+}
+
+?>
+
+</body>
+</html>
