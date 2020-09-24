4c0f1d9079a26f07f6a31b26deca7aff890f4f8a
Tanner Babcock <babkock@gmail.com>
2020-09-24 15:59:46 -0700

    Unit 6 Connection File test - Sensitive info Not Committed

---
+++ wdv341/dbTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+	Unit 6: Connection File
+	September 24, 2020
+	Tanner Babcock
+*/
+try {
+	require_once("dbConnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Unit 6: Connection File</title>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="/images/favicon.png" />
+	<link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+<body>
+	<h1>Unit 6: Connection File</h1>
+	<h3>tababcock_wdv341</h3>
+	<div class="box">
+		<table><thead>
+			<tr>
+				<td>ID</td>
+				<td>Event Name</td>
+				<td>Presenter</td>
+				<td>Date</td>
+			</tr>
+		</thead><tbody>
+<?php
+	foreach ($db->query("SELECT * from `wdv341_events`") as $row) {
+		$out = <<<EOF
+			<tr>
+				<td>{$row['event_id']}</td>
+				<td>{$row['event_name']}</td>
+				<td>{$row['event_presenter']}</td>
+				<td>{$row['event_date']}</td>
+			</tr>
+EOF;
+		echo $out;
+	}
+	?>
+		</tbody></table>
+	</div>
+	<p>I remembered not to commit my <b>dbConnect.php</b> file!</p>
+	<footer>
+		<a href="/homework/index">&rarr; Return to WDV341 Homework &larr;</a> &bull; <a href="https://github.com/Babkock/homework/blob/master/wdv341/dbTest.php" target="_blank">View Source Code</a>
+	</footer><?php
+}
+catch (PDOException $e) {
+	echo "<b>{$e->getMessage()}</b><br />";
+}
+finally {
+	?>
+</body>
+</html>
+	<?php
+	$db = null;
+}
