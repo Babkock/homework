f548d83b9fd9008d6f0c010dfbd3d2690962e04a
Tanner Babcock <babkock@gmail.com>
2020-10-13 15:01:17 -0700

    Please

--- wdv321/recipes/src/vue/recipe.vue
+++ wdv321/recipes/src/vue/recipe.vue
@@ -3,8 +3,8 @@
 		<h2>{{ this.recipes[myid].title }}</h2>
 		<img :src="this.recipes[myid].image" />
 		<p><b>Serves: {{ this.recipes[myid].serves }}</b></p>
-		<p><b>Preparation Time:</b> {{ this.recipes[myid].preparation.quantity }} {{ $vm0.recipes[myid].preparation.measurement }}</p>
-		<p><b>Cooking Time:</b> {{ this.recipes[myid].cooking.quantity }} {{ $vm0.recipes[myid].cooking.measurement }}</p>
+		<p><b>Preparation Time:</b> {{ this.recipes[myid].preparation.quantity }} {{ this.recipes[myid].preparation.measurement }}</p>
+		<p><b>Cooking Time:</b> {{ this.recipes[myid].cooking.quantity }} {{ this.recipes[myid].cooking.measurement }}</p>
 		<p><b>Difficulty:</b> {{ this.recipes[myid].difficulty }}</p>
 		<h3>Ingredients:</h3>
 		<ul v-if="this.recipes[myid].ingredients">
