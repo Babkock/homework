96eab41f90e99ade8facb1ebf29d24f2e10d12b1
Tanner Babcock <babkock@gmail.com>
2020-10-15 17:47:31 -0700

    Updates

--- wdv321/recipes/src/vue/ingredient.vue
+++ wdv321/recipes/src/vue/ingredient.vue
@@ -2,7 +2,7 @@
 	<tr class="ingredient" :id="myid">
 		<td>
 			#
-			<input type="text" :v-model="numVmodl" :value="myid" :title="title" :alt="title" :name="numName" :size="numSize" :maxlength="numSize" placeholder="#" @blur="$emit('numinput', $event.target.value)" />
+			<input type="text" :v-model="numVmodl" :value="id" :title="title" :alt="title" :name="numName" :size="numSize" :maxlength="numSize" placeholder="#" @blur="$emit('numinput', $event.target.value)" />
 		</td>
 		<td>
 			<select alt="Select a unit of measurement" title="Select a unit of measurement" :name="selName" :v-model="selVmodl" @change="$emit('measinput', $event.target.value)">
@@ -78,5 +78,67 @@ export default {
 </script>
 
 <style lang="scss">
-
+.ingredient {
+	width:100%;
+	margin-top:3px;
+	margin-bottom:3px;
+}
+.ingredient td {
+	background-color:#12121f;
+	padding:4px;
+	font-size:1.08em;
+	width:24%;
+	margin-left:1px;
+	margin-right:1px;
+	color:white;
+	input[type="text"] {
+		background-color: #121212 !important;
+		-moz-appearance: none;
+		appearance: none !important;
+		border: 1px solid #171717 !important;
+		padding: 5px;
+		color: #eee !important;
+		font-size: 1.08em;
+		margin:1px;
+		transition:background-color, color, border 0.2s ease 0s;
+		&:hover {
+			background-color: #202020 !important;
+			color:white !important;
+			border:1px solid #303d30 !important;
+		}
+	}
+	input:not(input[type="checkbox"]) {
+		-moz-appearance:none;
+		appearance:none;
+		color:white !important;
+		background-color:#121212 !important;
+		border:1px solid #171717 !important;
+		font-size:1.08em;
+		padding:5px;
+		width:80% !important;
+	}
+	select {
+		-moz-appearance:none;
+		appearance:none;
+		color:white;
+		padding:5px;
+		width:100% !important;
+		background-color:#121212 !important;
+		border:1px solid #192919;
+		&:hover {
+			background-color:#203020;
+			color:white !important;
+			border:1px solid #304130;
+		}
+		option {
+			padding:5px;
+			background-color:#121212 !important;
+			border:1px solid #191919;
+			&:hover {
+				background-color:green !important;
+				border:1px solid #192c19;
+			}
+		}
+	}
+}
 </style>
