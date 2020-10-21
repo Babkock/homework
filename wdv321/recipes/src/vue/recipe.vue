cd107e46355d5adde87aba9904b37aefaa0f4a07
Tanner Babcock <babkock@gmail.com>
2020-10-21 00:05:08 -0700

    Wed, Oct 21, 2020 12:05:08 AM

--- wdv321/recipes/src/vue/recipe.vue
+++ wdv321/recipes/src/vue/recipe.vue
@@ -1,23 +1,23 @@
 <template>
 	<div class="recipe" :id="myid">
-		<h2>{{ this.recipes[myid].title }}</h2>
+		<h2>{{ recipes[myid].title }}</h2>
 		<div class="imgbox">
-			<img :src="this.recipes[myid].image" />
+			<img :src="recipes[myid].image" />
 		</div>
-		<p><b>Serves: {{ this.recipes[myid].serves }}</b></p>
-		<p><b>Preparation Time:</b> {{ this.recipes[myid].preparation.quantity }} {{ this.recipes[myid].preparation.measurement }}</p>
-		<p><b>Cooking Time:</b> {{ this.recipes[myid].cooking.quantity }} {{ this.recipes[myid].cooking.measurement }}</p>
-		<p><b>Difficulty:</b> {{ this.recipes[myid].difficulty }}</p>
+		<p><b>Serves: {{ recipes[myid].serves }}</b></p>
+		<p><b>Preparation Time:</b> {{ recipes[myid].preparation.quantity }} {{ this.recipes[myid].preparation.measurement }}</p>
+		<p><b>Cooking Time:</b> {{ recipes[myid].cooking.quantity }} {{ this.recipes[myid].cooking.measurement }}</p>
+		<p><b>Difficulty:</b> {{ recipes[myid].difficulty }}</p>
 		<h3>Ingredients:</h3>
-		<ul v-if="this.recipes[myid].ingredients">
-			<li v-for="(ing, index) in this.recipes[myid].ingredients" :key="index">
+		<ul v-if="recipes[myid].ingredients">
+			<li v-for="(ing, index) in recipes[myid].ingredients" :key="index">
 				<b v-text="ing.quantity"></b> {{ ing.measurement }} {{ ing.name }}
 				<i v-if="ing.optional">(Optional)</i>
 			</li>
 		</ul>
 		<br />
-		<ol v-if="this.recipes[myid].steps">
-			<li v-for="(step, index) in this.recipes[myid].steps" :key="index">
+		<ol v-if="recipes[myid].steps">
+			<li v-for="(step, index) in recipes[myid].steps" :key="index">
 				{{ step }}
 			</li>
 		</ol>
