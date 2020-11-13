e373e40edbee2fb9ef7e4f2b2039a9552d6a35b1
Tanner Babcock <babkock@gmail.com>
2020-11-12 19:06:31 -0800

    Thu, Nov 12, 2020  7:06:30 PM

--- wdv321/react/src/app.js
+++ wdv321/react/src/app.js
@@ -6,9 +6,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = `
+const template = 
+<div>
 	<h2>Hello Reacting World!</h2>
 	<p>This is my very first React app compiled with Webpack.</p>
-`;
+</div>
+
 ReactDOM.render(template, document.getElementById('app'));
 
