c557373a640a230b792d7c750e77fa1c4a3cdcb4
Tanner Babcock <babkock@gmail.com>
2020-11-29 02:06:09 -0800

    Sun, Nov 29, 2020  2:06:08 AM

--- wdv341/wax/src/js/waxAccount.js
+++ wdv341/wax/src/js/waxAccount.js
@@ -40,7 +40,7 @@ let app = new Vue({
 			if (mode === "inventory") {
 				this.$http.post("user?mode=inventory", userId).then((response) => {
 					this.inventory = response.data;
-					console.log(this.inventoryAjax);
+					console.log(this.inventory);
 				}, () => {
 					this.inventory = "<p class=\"error\">Could not fetch this user's inventory from the server.</p>";
 				});
@@ -48,7 +48,7 @@ let app = new Vue({
 			else if (mode === "purchased") {
 				this.$http.post("user?mode=purchased", userId).then((response) => {
 					this.purchased = response.data;
-					console.log(this.purchasedAjax);
+					console.log(this.purchased);
 				}, () => {
 					this.purchased = "<p class=\"error\">Could not fetch this user's purchased albums from the server.</p>";
 				});
