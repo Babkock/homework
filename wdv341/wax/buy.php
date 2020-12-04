bfc7fb25c365e6da510c02154b61129a985d7d7a
Tanner Babcock <babkock@gmail.com>
2020-12-04 02:59:38 -0800

    Working on buy.php

--- wdv341/wax/buy.php
+++ wdv341/wax/buy.php
@@ -11,12 +11,67 @@ Methods::authorize();
 
 try {
 	if (!empty($_POST)) {
+		if (!isset($_POST['choice'])) {
+			exit("<p class=\"error\">Sorry, can't buy this album.</p>");
+		}
+		else if (strcmp($_POST['choice'], "Buy This Album") == 0) {
+			if (!isset($_GET['id'])) {
+				exit("<p class=\"error\">No 'id' argument given.</p>");
+			}
+			else {
+				$album = new Album();
+				$album->read(intval($_GET['id']));
 
+				$album->purchase($_SESSION['current_user']);
+
+				exit("<p class=\"error\">You have purchased this album! Thank you for your business. <a href=\"index\">View your purchases here.</a></p>");
+			}
+		}
 	}
 	else {
 		$buy = new Page("header_user", "buy_confirm");
 		$buy->setTitle("Buying {{ALBUM_TITLE}}");
 		$buy->setDescription("This is the purchase confirmation page for the album {{ALBUM_ARTIST}} - {{ALBUM_TITLE}}. It costs $ {{ALBUM_PRICE}}.");
+		if (!isset($_GET['id'])) {
+			$buy->error("No 'id' argument given.");
+			exit();
+		}
+		else {
+			$album = new Album();
+			$album->read(intval($_GET['id']));
+			$uid = Methods::getIdFromName($_SESSION['current_user']);
+
+			$tl = json_decode($album->getTracklist());
+			$tlout = "";
+
+			foreach ($tl as $k => $v) {
+				$tlout .= "<li>" . $v->title . " <i>(" . $v->length . ")</i></li>\n\t\t\t";
+			}
+
+			$buy->hreplacea([
+				"USERID" => $uid,
+				"ALBUM_TITLE" => $album->getTitle(),
+				"ALBUM_ARTIST" => $album->getArtist(),
+				"ALBUM_PRICE" => $album->getPrice()
+			]);
+			$buy->replacea([
+				"USERNAME" => $_SESSION['current_user'],
+				"ALBUM_ID" => $album->getId(),
+				"ALBUM_HREF" => "browse?b=" . urlencode(ucwords($album->getTitle())),
+				"ARTIST_HREF" => "browse?a=" . urlencode(ucwords($album->getArtist())),
+				"ALBUM_TITLE" => $album->getTitle(),
+				"ALBUM_ARTIST" => $album->getArtist(),
+				"ALBUM_MEDIA" => $album->getMedia(),
+				"ALBUM_PRICE" => $album->getPrice(),
+				"ALBUM_IMAGE" => $album->getImage(),
+				"ALBUM_DISCS" => $album->getDiscs(),
+				"ALBUM_LABEL" => $album->getLabel(),
+				"ALBUM_COUNTRY" => $album->getCountry(),
+				"ALBUM_SELLER" => $album->getSeller(),
+				"ALBUM_TRACKS" => $tlout
+			]);
+		}
+		$buy->output();
 	}
 }
 catch (PDOException $e) {
---
+++ wdv341/wax/contact.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	contact.php
+
+	WaXchange
+	November - December 2020
+	Copyright (c) 2020 Tanner Babcock.
+*/
+require_once("lib/waxchange.php");
+
+try {
+
+}
+catch (PDOException $ex) {
+
+}
